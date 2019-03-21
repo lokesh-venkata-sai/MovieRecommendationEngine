89203f9d8a0807434e7a3c61df0514170fc49ca0
lokesh-venkata-sai <mamidil.sai@st.niituniversity.in>
2019-03-22 00:58:13 +0530

    changes in register page

--- main.py
+++ main.py
@@ -3,18 +3,15 @@ from login import login
 from register import register
 import os
 from homefile import homefile
-
+from datetime import timedelta
 
 app = Flask(__name__)
 
 #app.config.from_pyfile("config.cfg")
-app.secret_key=os.urandom(24)
-
 
 @app.route('/')
 @app.route('/<login>')
 def index(login=None):
-    session.pop('mailId', None)
     return render_template("index.html",login=login)
 
 
@@ -22,26 +19,27 @@ def index(login=None):
 def signup():
     return render_template("signup.html")
 
-@app.route('/home')
-def home():
-    if g.mailId:
-        print("home ",g.mailId)
-        y=homefile()
-        results=y.homefunc()
-        return render_template('home.html',results=results)
-    return redirect(url_for('index'))
+
 
 @app.route('/loginForm',methods = ['GET','POST'])
 def loginForm():
     result=request.form
     x = login()
     y=x.validateLogin(**result)
-    if y==True:
-        session.pop('mailId',None)
-        session['mailId'] = result['mailId']
-        return redirect(url_for('home'))
-    else:
-        return render_template("index.html",login=True)
+    if request.method == 'POST':
+        session.pop('mail', None)
+        if y==True:
+            session['mail'] = result['mailId']
+            return redirect(url_for('home'))
+    return render_template("index.html",login=True)
+
+@app.route('/home')
+def home():
+    if g.mail:
+        y=homefile()
+        results=y.homefunc()
+        return render_template('home.html',results=results)
+    return redirect(url_for('index'))
 
 
 
@@ -60,33 +58,36 @@ def registerForm():
 
 @app.route('/myprofile')
 def myprofile():
-    if g.mailId:
-        print(session.get("mailId"))
-        user=session.get("mailId")
+    if g.mail:
+        #print(session.get("mailId"))
+        user=session.get("mail")
         return render_template("myprofile.html",user=user)
     return redirect(url_for('index'))
 
 
 @app.before_request
 def before_request():
-    g.mailId=None
-    if 'mailId' in session:
-        g.mailId=session['mailId']
-        print("hi ",g.mailId)
+    g.mail=None
+    if 'mail' in session:
+        g.mail=session['mail']
+        print("hi ",g.mail)
+
 
 @app.route('/getsession')
 def getsession():
-    if 'mailId' in session:
-        return session['mailId']
+    if 'mail' in session:
+        return session['mail']
     return 'Not logged in!'
 
 @app.route('/dropsession')
 def dropsession():
-    session.pop('mailId', None)
+    session.pop('mail', None)
     return render_template("index.html")
 
 
 
 
 if __name__=="__main__":
+    app.secret_key = os.urandom(24)
+    app.permanent_session_lifetime = timedelta(minutes=10)
     app.run(debug=True)
