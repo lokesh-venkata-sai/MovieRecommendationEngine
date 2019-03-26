7790f2c43559d54f731776acf4ce3dc2b69f5b45
Sreekar <bollamsreekhar@gmail.com>
2019-03-26 18:58:20 +0530

    features_genres list is convereted into different columns

--- features.py
+++ features.py
@@ -32,12 +32,21 @@ def features(x):
     return np.around(list_feature, decimals=2)
 
 
+features_matrix = np.arange(20).reshape((1, 20))
+
+
+def generate_features_matrix(x):
+    global features_matrix
+    x.reshape((1, 20))
+    features_matrix = np.vstack([features_matrix, x])
+
+
 def get_features():
     dir_ref = pathlib.Path('the-movies-dataset')
     if not (dir_ref.is_dir() and dir_ref.exists()):
         zip_ref = zipfile.ZipFile('the-movies-dataset.zip', 'r')
         zip_ref.extractall('./')
-    movies_metadata = pd.read_csv('./the-movies-dataset/movies_metadata.csv')
+    movies_metadata = pd.read_csv('./the-movies-dataset/movies_metadata.csv', low_memory=False)
     movies_metadata.drop(movies_metadata[movies_metadata.id == "1997-08-20"].index, inplace=True)
     movies_metadata.drop(movies_metadata[movies_metadata.id == "2012-09-29"].index, inplace=True)
     movies_metadata.drop(movies_metadata[movies_metadata.id == "2014-01-01"].index, inplace=True)
@@ -53,8 +62,14 @@ def get_features():
     for index, item in enumerate(set_genres):
         dict_genres[item] = index
     movies_metadata["features_genres"] = movies_metadata["genres_list"].apply(features)
+    movies_metadata.dropna(subset=['features_genres'], inplace=True)
     features_dataframe = movies_metadata.loc[:, ['id', 'features_genres']]
     features_dataframe.set_index(['id'], inplace=True)
     movie_ids = movies_metadata['id'].values
     movie_ids = movie_ids.astype(np.int)
-    return features_dataframe,movie_ids,np.array(list(set_genres))
+    features_dataframe.features_genres.apply(generate_features_matrix)
+    features_dataframe = pd.DataFrame(data=features_matrix[1:, :], columns=features_matrix[0, :], index=movie_ids)
+    features_dataframe.index.name = 'id'
+    features_dataframe.columns = features_dataframe.columns.astype('int32')
+    features_dataframe.index = features_dataframe.index.astype('int32')
+    return features_dataframe, movie_ids, np.array(list(set_genres))
