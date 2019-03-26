2fc4b96c7b8eca0acc361a5af37f93baae2a3f57
Sreekar <bollamsreekhar@gmail.com>
2019-03-26 18:59:25 +0530

    code refactoring

--- register.py
+++ register.py
@@ -1,10 +1,12 @@
 import pymysql
+
+
 class register():
-    def registerfunc(self,**result):
-        self.username=result['username']
-        self.mailId= result['mailId']
-        self.password=result['password']
-        self.confpPassword=result['confirmPassword']
+    def registerfunc(self, **result):
+        self.username = result['username']
+        self.mailId = result['mailId']
+        self.password = result['password']
+        self.confpPassword = result['confirmPassword']
 
         # Open database connection
         db = pymysql.connect("localhost", "root", "lokesh1999", "movieRecommendataion")
@@ -14,13 +16,13 @@ class register():
         # execute SQL query using execute() method.
 
         sql = "insert into users values(Null,%s,%s,aes_encrypt(%s,'passkey'))"
-        val =(self.username,self.mailId,self.password)
+        val = (self.username, self.mailId, self.password)
         # cursor.execute("SELECT VERSION()")
         # Fetch a single row using fetchone() method.
         try:
 
             # Execute the SQL command
-            cursor.execute(sql,val)
+            cursor.execute(sql, val)
             print("command executed")
             # Fetch all the rows in a list of lists.
             db.commit()
@@ -32,4 +34,3 @@ class register():
             db.close()
             return False
         return True
-
