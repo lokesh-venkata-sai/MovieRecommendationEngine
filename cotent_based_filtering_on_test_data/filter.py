34ae123e9e77bda8b2d3695cf1dbaabdfd9d0aed
Sreekar <bollamsreekhar@gmail.com>
2019-03-27 17:16:58 +0530

    commented the debugging print statements

--- cotent_based_filtering_on_test_data/filter.py
+++ cotent_based_filtering_on_test_data/filter.py
@@ -1,3 +1,4 @@
+import numpy as np
 from cotent_based_filtering_on_test_data.features import get_features
 from cotent_based_filtering_on_test_data.Ratings_matrix import get_ratings_matrix
 
@@ -11,28 +12,28 @@ def filter():
     movies_common = list(movies_common)
     ratings = ratings.loc[movies_common, :]
     features_dataframe = features_dataframe.loc[movies_common, :]
-    return features_dataframe.values[1:, :], ratings.values[:, 1:], movies_common, user_ids, list_genres
+    return features_dataframe.values[1:, :], ratings.values[:, 1:], np.array(movies_common), user_ids, list_genres
 
 
-feature_vector, ratings_vector, movies_ids, user_ids, list_genres = filter()
-print(feature_vector)
-print("The above matrix are 20 different genres features for 2800 movies")
-print()
-print("the feature vector representation of movieId " + str(movies_ids[0]))
-print(feature_vector[0])
-print()
-print(list_genres)
-print("These are the genres considered for each movie")
-
-
-print()
-print()
-print()
-print()
-
-
-print(ratings_vector)
-print("The above matrix are ratings given by 671 users for 2800 movies")
-print()
-print("these are ratings given by 671 users for movieId " + str(movies_ids[0]))
-print(ratings_vector[0])
+# feature_vector, ratings_vector, movies_ids, user_ids, list_genres = filter()
+# print(feature_vector)
+# print("The above matrix are 20 different genres features for 2800 movies")
+# print()
+# print("the feature vector representation of movieId " + str(movies_ids[0]))
+# print(feature_vector[0])
+# print()
+# print(list_genres)
+# print("These are the genres considered for each movie")
+#
+#
+# print()
+# print()
+# print()
+# print()
+#
+#
+# print(ratings_vector)
+# print("The above matrix are ratings given by 671 users for 2800 movies")
+# print()
+# print("these are ratings given by 671 users for movieId " + str(movies_ids[0]))
+# print(ratings_vector[0])
