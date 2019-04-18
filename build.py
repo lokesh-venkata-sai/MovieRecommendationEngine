af8b66ae06dd20f2ed09689a0a5b763def8850fe
lokesh-venkata-sai <mamidil.sai@st.niituniversity.in>
2019-04-18 13:50:26 +0530

    docker file and build file added

---
+++ build.py
@@ -0,0 +1,8 @@
+from pybuilder.core import use_plugin
+
+use_plugin("python.core")
+use_plugin("python.unittest")
+use_plugin("python.coverage")
+use_plugin("python.distutils")
+
+default_task = "publish"
