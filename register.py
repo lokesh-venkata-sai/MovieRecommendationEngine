e8cd0b49f9ab3c9c01beccb84adebcbcb58a365f
lokesh-venkata-sai <mamidil.sai@st.niituniversity.in>
2019-04-28 00:45:56 +0530

    removed print statements

--- register.py
+++ register.py
@@ -19,7 +19,7 @@ class register():
         self.Romance=0
         self.Sci_Fi=0
         self.Thriller=0
-        print("genre: ",genre)
+        #print("genre: ",genre)
         for item in genre:
             if item=="Action":
                 self.Action=1
@@ -42,8 +42,7 @@ class register():
             if item=="Thriller":
                 self.Thriller=1
 
-        print(self.name,self.mailId,self.password,self.Action,self.Adventure,self.Animation,self.Comedy,self.Drama,self.Fantasy,self.Horror,
-            self.Romance,self.Sci_Fi,self.Thriller)
+        #print(self.name,self.mailId,self.password,self.Action,self.Adventure,self.Animation,self.Comedy,self.Drama,self.Fantasy,self.Horror,self.Romance,self.Sci_Fi,self.Thriller)
         # Open database connection
         db = pymysql.connect(mysql_server, "root", "lokesh1999", "movieRecommendation")
         # prepare a cursor object using cursor() method
@@ -54,10 +53,10 @@ class register():
         sql = "insert into users values(Null,%s,%s,aes_encrypt(%s,'passkey'),%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
         val =(self.name,self.mailId,self.password,int(self.Action),int(self.Adventure),int(self.Animation),int(self.Comedy),int(self.Drama),int(self.Fantasy),int(self.Horror),int(self.Romance),int(self.Sci_Fi),int(self.Thriller))
         try:
-            print("in try")
+            #print("in try")
             # Execute the SQL command
             cursor.execute(sql,val)
-            print("command executed")
+            #print("command executed")
             # Fetch all the rows in a list of lists.
         except Exception as e:
             print(e)
