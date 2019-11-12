552a1b9e48a73bcd0a4cb6ecb33a32cc70294f01
lokesh-venkata-sai <mamidil.sai@st.niituniversity.in>
2019-11-13 00:40:49 +0530

    Added source directory

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -8,10 +8,9 @@ import pymysql
 from interestfile import update
 from moviesfile import moviefile
 from singleMoviefile import singleMovie
-from ratingfile import ratingfile
 app = Flask(__name__)
 
-mysql_server="localhost"
+
 # app.config.from_pyfile("config.cfg")
 @app.route('/')
 @app.route('/<login>')
@@ -40,28 +39,9 @@ def loginForm():
 @app.route('/home')
 def home():
     if g.mail:  # to check if logged in
-        user_id = ""
-        db = pymysql.connect(mysql_server, "root", "lokesh1999", "movieRecommendation")
-        cursor = db.cursor()
-        sql = "select id from users where email=%s"
-        value = (g.mail)
-        try:
-            # Execute the SQL command
-            cursor.execute(sql, value)
-            # Fetch all the rows in a list of lists.
-            movie = cursor.fetchall()
-            user_id = movie[0][0]
-        except:
-            print("Error: unable to fetch data")
-        db.close()
-        y = homefile()
+        y = homefile()  # not yet implemented
         results = y.homefunc()
-        recommend=y.getrecommend(user_id)
-        if recommend == None:
-            print("none")
-            return render_template('home.html',results=results,novalue="")
-
-        return render_template('home.html', results=results, results1=recommend)
+        return render_template('home.html', results=results)
     return redirect(url_for('index'))
 
 @app.route('/movies',)
@@ -79,7 +59,7 @@ def searchForm():
         search=inp['searching']
         movies=""
         if search!="":
-            db = pymysql.connect(mysql_server, "root", "lokesh1999", "movieRecommendation")
+            db = pymysql.connect("localhost", "root", "lokesh1999", "movieRecommendataion")
             cursor = db.cursor()
             sql = "select * from movies where movie like %s"
             x="%"+inp['searching']+"%"
@@ -120,7 +100,7 @@ def registerForm():
 @app.route('/single/<ID>')
 def single(ID):
     if g.mail:
-        db = pymysql.connect(mysql_server, "root", "lokesh1999", "movieRecommendation")
+        db = pymysql.connect("localhost", "root", "lokesh1999", "movieRecommendataion")
         cursor = db.cursor()
         sql = "select * from movies where ID=%s"
         value = (ID)
@@ -134,66 +114,21 @@ def single(ID):
         db.close()
         obj=singleMovie()
         genre=obj.getGenre(*movie)
-        rating=obj.getrating(g.mail,ID)
-        avg=obj.getAvgRating(ID)
 
-        user_id = ""
-        db = pymysql.connect(mysql_server, "root", "lokesh1999", "movieRecommendation")
-        cursor = db.cursor()
-        sql = "select id from users where email=%s"
-        value = (g.mail)
-        try:
-            # Execute the SQL command
-            cursor.execute(sql, value)
-            # Fetch all the rows in a list of lists.
-            res = cursor.fetchall()
-            user_id = res[0][0]
-        except:
-            print("Error: unable to fetch data")
-        db.close()
-        y = homefile()
-        results = y.homefunc()
-        recommend = y.getrecommend(user_id)
-        if recommend == None:
-            return render_template("singleMovie.html",movie=movie,genre=genre,id=ID,rating=rating,avg=avg,results1=recommend)
-
-        return render_template("singleMovie.html",movie=movie,genre=genre,id=ID,rating=rating,avg=avg,results1=recommend)
+    return render_template("singleMovie.html",movie=movie,genre=genre)
 
 @app.route('/rating',methods=['GET', 'POST'])
 def rating():
     if g.mail:
-        user_id=""
-
-        db = pymysql.connect(mysql_server, "root", "lokesh1999", "movieRecommendation")
-        cursor = db.cursor()
-        sql = "select id from users where email=%s"
-        value = (g.mail)
-        try:
-            # Execute the SQL command
-            cursor.execute(sql, value)
-            # Fetch all the rows in a list of lists.
-            movie = cursor.fetchall()
-            user_id=movie[0][0]
-        except:
-            print("Error: unable to fetch data")
-        db.close()
         result=request.form
-        if "star" in result:
-            print(result["star"])
-            print(result["movie_id"])
-            print("user=",user_id)
-            obj=ratingfile
-            #status=obj.ratingfunc(user_id,result["movie_id"],result["star"])
-            status=obj.ratingfunc(obj,user_id,**result)
-            if status==True:
-                return redirect(url_for("home"))
+        print(result["star"])
         return redirect(url_for("home"))
 
 @app.route('/myprofile', methods=['GET', 'POST'])
 def myprofile():
     if g.mail:
         user = session.get("mail")
-        db = pymysql.connect(mysql_server, "root", "lokesh1999", "movieRecommendation")
+        db = pymysql.connect("localhost", "root", "lokesh1999", "movieRecommendataion")
         cursor = db.cursor()
         sql = "select * from users where email=%s"
         value = (session.get("mail"))
@@ -225,7 +160,7 @@ def before_request():
     g.mail = None
     if 'mail' in session:
         g.mail = session['mail']
-
+        print("hi ", g.mail)
 
 
 @app.route('/getsession')
@@ -244,4 +179,5 @@ def dropsession():
 if __name__ == "__main__":
     app.secret_key = os.urandom(24)
     app.permanent_session_lifetime = timedelta(minutes=10)
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0")
